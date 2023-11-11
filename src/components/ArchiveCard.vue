3c7ccc8c4ea91ace0671eaf28c1d93705d4f2948
r.trofimenko <r.trofimenko@net-vision.pro>
2023-11-11 22:52:26 +0300

    added date

--- src/components/ArchiveCard.vue
+++ src/components/ArchiveCard.vue
@@ -1,13 +1,20 @@
 <template>
   <n-card hoverable style="cursor: pointer">
-    <p>{{props.archiveItem.name}}</p>
+    <p>Название файла{{props.archiveItem.name}}</p>
+    <small>Дата: {{date}}</small>
   </n-card>
 </template>
 
 <script setup lang="ts">
 
+import {computed} from "vue";
+
 const props = defineProps<{archiveItem: Record<string, any>}>()
 
+
+const date = computed(() => {
+  return new Date(props.archiveItem.created).toLocaleString()
+})
 </script>
 
 <style lang="scss">
